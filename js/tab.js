2c4ef3cdf438f18ce528826e35f7113f7a40a76e
vincentmilliken <vincent.milliken@booking.com>
2017-11-09 13:50:55 +0100

    https

--- js/tab.js
+++ js/tab.js
@@ -6,7 +6,7 @@ $(function() {
 
         $("body").css("background-image", "url(img/clear.gif)");
         $("#bug").css("display", "block");
-        $.getJSON("http://tv.giphy.com/v1/gifs/screensaver?api_key=wBL1FvTTel6OQ&tag=giphytrending", null, function(response) {
+        $.getJSON("https://tv.giphy.com/v1/gifs/screensaver?api_key=wBL1FvTTel6OQ&tag=giphytrending", null, function(response) {
             var image = response.data.image_original_url;
             var gifId = response.data.id;
             $("#gif").css("background-image", "url(" + image + ")");
